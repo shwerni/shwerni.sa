7d69e60ffb198747e05e8151e489b37b11467fed
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2026-02-19 04:30:22 +0200

    main

--- app/(pages)/(management)/zadmin/collaboration/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Page = () => {
-  return <div>collaborations</div>;
-};
-
-export default Page;
--- app/(pages)/(management)/zadmin/statistics/loading.tsx
+++
@@ -1,9 +0,0 @@
-// React & Next
-import React from "react";
-
-// components
-import StatisticsLoading from "@/app/_components/layout/skeleton/admin/statistics";
-
-export default function loading() {
-  return <StatisticsLoading dir="ltr" />;
-}
