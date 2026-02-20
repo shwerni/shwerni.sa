a710b35a1cf2a548727e135df1f21e8d38d7af93
Ziad Aboalmjd <ziadgamalaboalmajd@gmail.com>
2026-02-20 13:21:54 +0200

    main

---
+++ app/(pages)/(site)/(deprecated)/meeting/[zid]/page.tsx
@@ -0,0 +1,22 @@
+"use server";
+// React & Next
+import { Metadata } from "next";
+import { permanentRedirect } from "next/navigation";
+
+// props
+type Props = {
+  params: Promise<{ zid: string }>;
+};
+
+// meta data seo
+export const metadata: Metadata = {
+  title: "شاورني - الاجتماع",
+  description: "shwerni payment - شاورني صفحة الاجتماع",
+};
+
+// return
+export default async function OwnerConultant({ params }: Props) {
+  const { zid } = await params;
+
+  permanentRedirect(`/meetings/${zid}`);
+}
