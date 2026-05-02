7041c7aec7773fec210e3b215c646ba8797f097d
shwerni <shwerni.sa@gmail.com>
2026-05-02 19:15:10 +0300

    Update reserve.tsx

--- components/clients/consultants/reservation/reserve.tsx
+++ components/clients/consultants/reservation/reserve.tsx
@@ -43,21 +43,21 @@ const ConsultantReserve = async ({ cid, collaboration }: Props) => {
   if (!cost || !finance || !info) return;
 
   // discount // dynamic later
-  const isDiscount = info?.DiscountConsultant.some(
-    (i) => i.discountId === 3 && i.status,
-   );
+  // const isDiscount = info?.DiscountConsultant.some(
+  //  (i) => i.discountId === 3 && i.status,
+  // );
 
   // cost
-  if (isDiscount) cost[30] = 77.3;
+  // if (isDiscount) cost[30] = 77.3;
 
   return (
     <div className="max-w-6xl mx-auto py-5 space-y-8">
-      {/* discount badge */}
+      {/* discount badge 
        {isDiscount && (
         <div className="w-fit mx-auto">
           <DiscountBadge />
         </div>
-      )}
+      )} */}
       {/* reservation */}
       <Reservation
         cid={cid}
